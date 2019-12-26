0f6d869480aa341d3c548e4911c7f11d3417ce95
Brian Skruch <brian@brian.skruch.com>
2019-12-26 03:01:41 -0600

    After Section 9 Lecture 64

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,6 +2,8 @@ import React, { useReducer } from 'react';
 import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import setAuthToken from '../../utils/setAuthToken';
+
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
@@ -25,7 +27,22 @@ const AuthState = props => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   // Load User
-  const loadUser = () => console.log('loaduser');
+  const loadUser = async () => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+
+    try {
+      const res = await axios.get('api/auth');
+
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({ type: AUTH_ERROR });
+    }
+  };
 
   // Register User
   const register = async formData => {
@@ -41,6 +58,8 @@ const AuthState = props => {
         type: REGISTER_SUCCESS,
         payload: res.data
       });
+      
+      loadUser();
     } catch (err) {
       dispatch({
         type: REGISTER_FAIL,
@@ -55,7 +74,7 @@ const AuthState = props => {
   const logout = () => console.log('logout');
 
   // Clear Errors
-  const clearErrors = () => dispatch({type: CLEAR_ERRORS});
+  const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
   return (
     <AuthContext.Provider
